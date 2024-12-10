b466dfc1187e0b692d6627724a6eceb3e33847f0
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-10 23:19:54 +0100

    db connection pool setup

---
+++ server/src/database/pg.client.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { Pool } from "pg";
+
+const {
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_HOST,
+  POSTGRES_DATABASE,
+  POSTGRES_PORT,
+  POSTGRES_TEST_DATABASE,
+} = process.env;
+
+const poolConfig = {
+  connectionString: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST || "localhost"}:${POSTGRES_PORT ? parseInt(POSTGRES_PORT, 10) : 5432}/${process.env.NODE_ENV === "test" ? POSTGRES_DATABASE : POSTGRES_TEST_DATABASE}`,
+};
+
+const pool = new Pool(poolConfig);
+
+export { pool };
