2b06bd74e074f360b99feae4675717ddeb931d94
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-10 23:23:40 +0100

    router setup

--- server/src/index.app.ts
+++ server/src/index.app.ts
@@ -1,8 +1,11 @@
 import express, { json } from "express";
+import router from "./routers/index.routers";
 
 const app = express();
 
 app.use(json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 export { app };
