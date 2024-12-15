db112ebeaef8ca475d16779e55e27f18268e2e93
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-15 23:20:15 +0100

    Entity Datamapper Requirements interface creation

---
+++ server/src/datamappers/interfaces/EntityDatamapperReq.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Pool } from "pg";
+import { TableNames } from "../../helpers/TableNames";
+
+export interface EntityDatamapperReq {
+  tableName: TableNames;
+  pool: Pool;
+  data: any;
+  findByPk(id: number): Promise<EntityDatamapperReq["data"]>;
+  findAll(): Promise<EntityDatamapperReq["data"][]>;
+  findBySpecificField(
+    field: string,
+    value: string
+  ): Promise<EntityDatamapperReq["data"]>;
+  insert(
+    item: EntityDatamapperReq["data"]
+  ): Promise<EntityDatamapperReq["data"]>;
+  update(
+    item: EntityDatamapperReq["data"]
+  ): Promise<EntityDatamapperReq["data"]>;
+  delete(id: number): Promise<EntityDatamapperReq["data"]>;
+}
