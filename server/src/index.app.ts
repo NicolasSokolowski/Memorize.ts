b571cb4485907717fc39afba1fe69c16bae19662
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-15 23:06:32 +0100

    Error handler setup

--- server/src/index.app.ts
+++ server/src/index.app.ts
@@ -1,4 +1,5 @@
 import express, { json } from "express";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
 import router from "./routers/index.routers";
 
 const app = express();
@@ -7,5 +8,7 @@ app.use(json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
+
+app.use(errorHandler);
 
 export { app };
