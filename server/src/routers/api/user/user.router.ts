8fa77c7329a8bd2b678c00626cb41d32c24bc99d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-16 16:51:09 +0100

    feat: get all users route

--- server/src/routers/api/user/user.router.ts
+++ server/src/routers/api/user/user.router.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { errorCatcher } from "../../../helpers/errorCatcher.helper";
 import { userController } from "../../../controllers/index.controllers";
 import roleRouter from "./role.router";
+import { requireAuth } from "../../../middlewares/requireAuth.helper";
 
 const userRouter = express.Router();
 
 userRouter
   .route("/")
-  .get(errorCatcher(userController.getAll))
+  .get(errorCatcher(requireAuth), errorCatcher(userController.getAll))
   .post(errorCatcher(userController.signup));
 
 userRouter.use("/role", roleRouter);
