01e283a2c7c19e77854ee45ab069b9ae29a10a17
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-01 23:35:29 +0100

    Added find all decks by user ID method

--- server/src/datamappers/DeckDatamapper.ts
+++ server/src/datamappers/DeckDatamapper.ts
@@ -6,4 +6,13 @@ import { DeckDatamapperReq } from "./interfaces/DeckDatamapperReq";
 export class DeckDatamapper extends CoreDatamapper<DeckDatamapperReq> {
   readonly tableName = TableNames.Deck;
   pool = pool;
+
+  findAllDecksByUserId = async (id: number) => {
+    const result = await this.pool.query(
+      `SELECT * FROM "${this.tableName}" WHERE "user_id" = $1`,
+      [id]
+    );
+
+    return result.rows;
+  };
 }
