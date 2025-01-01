43b7b3ce868e49fb71c3febca881061d10f1c968
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-01 17:00:41 +0100

    CardController creation

--- server/src/controllers/index.controllers.ts
+++ server/src/controllers/index.controllers.ts
@@ -2,13 +2,16 @@ import {
   roleDatamapper,
   userDatamapper,
   deckDatamapper,
+  cardDatamapper,
 } from "../datamappers/index.datamappers";
 import { RoleController } from "./RoleController";
 import { UserController } from "./UserController";
 import { DeckController } from "./DeckController";
+import { CardController } from "./CardController";
 
 const userController = new UserController(userDatamapper);
 const roleController = new RoleController(roleDatamapper);
 const deckController = new DeckController(deckDatamapper);
+const cardController = new CardController(cardDatamapper);
 
-export { userController, roleController, deckController };
+export { userController, roleController, deckController, cardController };
