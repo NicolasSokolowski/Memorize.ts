cfde325c0fb59027c598ee2c36474ace899a8df8
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-01 13:51:48 +0100

    Added deck creation, getByPk and update routes

--- server/src/routers/api/deck/deck.router.ts
+++ server/src/routers/api/deck/deck.router.ts
@@ -2,11 +2,28 @@ import express from "express";
 import { errorCatcher } from "../../../helpers/errorCatcher.helper";
 import { requireAuth } from "../../../middlewares/requireAuth.helper";
 import { deckController } from "../../../controllers/index.controllers";
+import { checkPermissions } from "../../../middlewares/checkPermissions.middleware";
 
 const deckRouter = express.Router();
 
 deckRouter
   .route("/")
-  .post(errorCatcher(requireAuth), errorCatcher(deckController.create));
+  .post(
+    errorCatcher(checkPermissions(["admin", "user"], "user")),
+    errorCatcher(deckController.create)
+  );
+
+deckRouter
+  .route("/:deck_id")
+  .get(
+    errorCatcher(requireAuth),
+    errorCatcher(checkPermissions(["admin", "user"], "deck")),
+    errorCatcher(deckController.getByPk)
+  )
+  .put(
+    errorCatcher(requireAuth),
+    errorCatcher(checkPermissions(["admin", "user"], "deck")),
+    errorCatcher(deckController.update)
+  );
 
 export default deckRouter;
