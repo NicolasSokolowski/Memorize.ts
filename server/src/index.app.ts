3323e312eb0e3f1b10461fb1af8656c1b898e4ad
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-06-06 01:43:19 +0200

    adding cookie-parser dep

--- server/src/index.app.ts
+++ server/src/index.app.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import router from "./routers/index.routers";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(json());
 app.use(express.urlencoded({ extended: true }));
 
