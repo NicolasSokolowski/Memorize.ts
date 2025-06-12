7ee91e12780e795d0c2a485d962b544e3bcaaf96
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-06-12 12:01:24 +0200

    Deck's update schema validation

--- server/src/validation/index.validation.ts
+++ server/src/validation/index.validation.ts
@@ -2,6 +2,7 @@ import roleCreateSchema from "./schemas/role/role.create.schema";
 import userRoleUpdateSchema from "./schemas/user/user.role.update.schema";
 import userCreateSchema from "./schemas/user/user.create.schema";
 import deckCreateSchema from "./schemas/deck/deck.create.schema";
+import deckUpdateSchema from "./schemas/deck/deck.update.schema";
 import cardCreateSchema from "./schemas/card/card.create.schema";
 import userUpdateSchema from "./schemas/user/user.update.schema";
 import passwordUpdateSchema from "./schemas/user/password.update.schema";
@@ -11,6 +12,7 @@ export {
   userRoleUpdateSchema,
   userCreateSchema,
   deckCreateSchema,
+  deckUpdateSchema,
   cardCreateSchema,
   userUpdateSchema,
   passwordUpdateSchema
