aec3481878419de2bd80f8526877868c145ad944
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-06-20 23:07:48 +0200

    update card progress function impl

--- server/src/helpers/index.helpers.ts
+++ server/src/helpers/index.helpers.ts
@@ -2,5 +2,6 @@ import { errorCatcher } from "./errorCatcher.helper";
 import { Password } from "./Password";
 import { UserPayload } from "./UserPayload.helper";
 import { Token } from "./Token";
+import { updateCardProgress } from "./updateCardProgress.helper";
 
-export { errorCatcher, Password, UserPayload, Token };
+export { errorCatcher, Password, UserPayload, Token, updateCardProgress };
