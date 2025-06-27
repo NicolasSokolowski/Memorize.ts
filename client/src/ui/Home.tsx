1fa7aa5ae27300fa0f04651d5131d1b4c82b3b47
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-06-28 00:57:45 +0200

    added logo and main title to Home page

--- client/src/ui/Home.tsx
+++ client/src/ui/Home.tsx
@@ -2,7 +2,14 @@ function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center gap-8 bg-primary">
       <div className="flex h-128 w-128 flex-col gap-8">
-        <div className="h-40 w-128"></div>
+        <div className="flex h-40 w-128 items-center justify-center">
+          <img
+            src="/logo.png"
+            alt="GrowMind Logo"
+            className="h-full object-contain"
+          />
+          <h1 className="mr-10 font-patua text-6xl text-tertiary">growMind</h1>
+        </div>
         <div className="h-80 w-128 rounded-md border-gray-300 bg-white shadow-inner-strong"></div>
       </div>
       <div className="h-128 w-100  rounded-md border-gray-300 bg-white shadow-inner-strong"></div>
