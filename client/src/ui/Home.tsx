cdedc32aacc0a898aecefff801168ea4a92bc98f
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-06-28 00:30:58 +0200

    Home page layout

--- client/src/ui/Home.tsx
+++ client/src/ui/Home.tsx
@@ -1,7 +1,11 @@
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="flex min-h-screen items-center justify-center gap-8 bg-primary">
+      <div className="flex h-128 w-128 flex-col gap-8">
+        <div className="h-40 w-128"></div>
+        <div className="h-80 w-128 rounded-md border-gray-300 bg-white shadow-inner-strong"></div>
+      </div>
+      <div className="h-128 w-100  rounded-md border-gray-300 bg-white shadow-inner-strong"></div>
     </div>
   );
 }
