3dec68d3ce31c12106fcdd98f4f7a4b86defa1ba
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-06-29 16:21:27 +0200

    added extra reducer to user slice to handle login progress

--- client/src/store/user/userSlice.ts
+++ client/src/store/user/userSlice.ts
@@ -1,13 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { login } from "./userThunk";
 
 export interface User {
   email: string;
   password: string;
   username: string;
   role_id: number;
-  last_login: Date;
+  last_login: Date | null;
   created_at: Date;
-  updated_at: Date;
+  updated_at: Date | null;
 }
 
 interface UserState {
@@ -29,6 +30,20 @@ const userSlice = createSlice({
     setHasAccount: (state, action) => {
       state.hasAccount = action.payload;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state) => {
+        state.isLoading = false;
+        state.user = null;
+      });
   }
 });
 
