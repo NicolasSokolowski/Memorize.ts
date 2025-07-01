1da09fd4f940a0917de7f23af920125de96e86da
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-02 01:47:34 +0200

    deck details component implementation

---
+++ client/src/features/deck/DeckDetails.tsx
@@ -0,0 +1,17 @@
+import { Deck } from "../../store/deck/deckSlice";
+
+interface DeckProps {
+  deck: Deck;
+}
+
+function DeckDetails({ deck }: DeckProps) {
+  return (
+    <div className="flex size-60 justify-center rounded-md bg-tertiary pt-3 shadow-xl">
+      <div className="flex h-[15%] w-full justify-center">
+        <h3 className="font-patua text-xl text-textPrimary">{deck.name}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default DeckDetails;
