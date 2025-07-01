7f36b115ae6ef428400d1aaf61a3fd118e3887fd
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-01 18:31:49 +0200

    added deck slice to the store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cardReducer from "./card/cardSlice";
 import userReducer from "./user/userSlice";
+import { deckReducer } from "./deck/deckSlice";
 
 const store = configureStore({
   reducer: {
+    deck: deckReducer,
     card: cardReducer,
     user: userReducer
   }
