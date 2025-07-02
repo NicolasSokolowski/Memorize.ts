605cc88184e21288fd4d80dd0d2a5f321f0e5cbd
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-02 20:51:42 +0200

    displaying navbar on the left side

--- client/src/ui/UserLayout.tsx
+++ client/src/ui/UserLayout.tsx
@@ -2,9 +2,13 @@ import { Outlet } from "react-router-dom";
 
 function UserLayout() {
   return (
-    <div>
-      <div>Replace with navbar component</div>
-      <Outlet />
+    <div className="flex h-screen">
+      <div className="z-10 w-60 bg-red-500 shadow-right">
+        Replace with navbar component
+      </div>
+      <div className="w-full grow overflow-hidden bg-primary">
+        <Outlet />
+      </div>
     </div>
   );
 }
