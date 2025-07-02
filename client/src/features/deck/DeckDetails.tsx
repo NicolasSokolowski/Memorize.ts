5e0fe8438e8b84fe011704de11ff514261c3cc24
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-02 14:03:32 +0200

    added background image to decks

--- client/src/features/deck/DeckDetails.tsx
+++ client/src/features/deck/DeckDetails.tsx
@@ -6,7 +6,7 @@ interface DeckProps {
 
 function DeckDetails({ deck }: DeckProps) {
   return (
-    <div className="flex size-60 justify-center rounded-md bg-tertiary pt-3 shadow-xl">
+    <div className="flex size-60 justify-center rounded-md bg-tertiary bg-[url('/deck.png')] bg-cover pt-3 shadow-xl">
       <div className="flex h-[15%] w-full justify-center">
         <h3 className="font-patua text-xl text-textPrimary">{deck.name}</h3>
       </div>
