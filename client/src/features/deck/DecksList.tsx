7099c322ed477b6ae21425df6315d37b97bb7225
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-03 18:09:53 +0200

    added button to create deck

--- client/src/features/deck/DecksList.tsx
+++ client/src/features/deck/DecksList.tsx
@@ -14,6 +14,11 @@ function DecksList() {
   return (
     <div className="h-full overflow-y-auto bg-primary p-12">
       <div className="grid grid-cols-5 gap-10 pb-8">
+        <button className="flex size-60 items-center justify-center rounded-lg bg-tertiary shadow-lg">
+          <span className="relative top-[-12px] font-patua text-9xl text-secondary">
+            +
+          </span>
+        </button>
         {decks.map((deck) => (
           <DeckDetails key={deck.id} deck={deck} />
         ))}
