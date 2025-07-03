776738c4d8c351284c01ca52aa09fa3ac21d685f
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-03 18:14:00 +0200

    moved deck creation box into it's own component

---
+++ client/src/features/deck/DeckCreation.tsx
@@ -0,0 +1,11 @@
+function DeckCreation() {
+  return (
+    <button className="flex size-60 items-center justify-center rounded-lg bg-tertiary shadow-lg">
+      <span className="relative top-[-12px] font-patua text-9xl text-secondary">
+        +
+      </span>
+    </button>
+  );
+}
+
+export default DeckCreation;
--- client/src/features/deck/DecksList.tsx
+++ client/src/features/deck/DecksList.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { getDecks } from "../../store/deck/deckThunk";
 import DeckDetails from "./DeckDetails";
+import DeckCreation from "./DeckCreation";
 
 function DecksList() {
   const dispatch = useAppDispatch();
@@ -14,11 +15,7 @@ function DecksList() {
   return (
     <div className="h-full overflow-y-auto bg-primary p-12">
       <div className="grid grid-cols-5 gap-10 pb-8">
-        <button className="flex size-60 items-center justify-center rounded-lg bg-tertiary shadow-lg">
-          <span className="relative top-[-12px] font-patua text-9xl text-secondary">
-            +
-          </span>
-        </button>
+        <DeckCreation />
         {decks.map((deck) => (
           <DeckDetails key={deck.id} deck={deck} />
         ))}
