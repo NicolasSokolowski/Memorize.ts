ef8c3495a2b64b2203b7926ca4812faafd181d7d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-03 18:20:55 +0200

    create deck thunk implementation

--- client/src/store/deck/deckThunk.ts
+++ client/src/store/deck/deckThunk.ts
@@ -6,3 +6,11 @@ export const getDecks = createAsyncThunk<Deck[]>("GET_DECKS", async () => {
   const response = await axiosInstance.get("/decks");
   return response.data as Deck[];
 });
+
+export const createDeck = createAsyncThunk<Deck, Partial<Deck>>(
+  "CREATE_DECK",
+  async (newDeck: Partial<Deck>) => {
+    const response = await axiosInstance.post("/decks", newDeck);
+    return response.data as Deck;
+  }
+);
