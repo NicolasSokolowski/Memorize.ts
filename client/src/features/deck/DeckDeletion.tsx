c4ebb0b5431b74e5eaade09b707bd60cc2cd5d7f
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-05 00:37:27 +0200

    form now handle deck deletion submit

--- client/src/features/deck/DeckDeletion.tsx
+++ client/src/features/deck/DeckDeletion.tsx
@@ -1,16 +1,43 @@
+import { deleteDeck } from "../../store/deck/deckThunk";
+import { useAppDispatch } from "../../store/hooks";
 import { DeckProps } from "./DeckDetails";
 
 interface DeckModificationProps extends DeckProps {
   onCancel: () => void;
 }
 
+interface ApiErrorResponse {
+  errors: {
+    message: string;
+    field?: string;
+  }[];
+}
+
 function DeckDeletion({ deck, onCancel }: DeckModificationProps) {
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = () => async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await dispatch(deleteDeck(deck.id)).unwrap();
+
+      onCancel();
+    } catch (err: unknown) {
+      const error = err as ApiErrorResponse;
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flip-box-b-right size-60 rounded-lg bg-tertiary shadow-lg">
       <div className="flex h-full flex-col justify-between">
         <h3 className="mt-4 text-center font-patua text-xl">Supprimer</h3>
         <div className="flex h-full flex-col items-center justify-center">
-          <form className="flex flex-col items-center gap-2">
+          <form
+            className="flex flex-col items-center gap-2"
+            onSubmit={handleSubmit()}
+          >
             <p className="w-44 pl-2 font-patua text-base text-textPrimary">
               Voulez-vous vraiment supprimer ?
             </p>
