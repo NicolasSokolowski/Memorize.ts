93f7095b4dee66f2ed2fa0699ca54fc306a2e2be
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-04 15:48:16 +0200

    added modification icon on decks

--- client/src/features/deck/DeckDetails.tsx
+++ client/src/features/deck/DeckDetails.tsx
@@ -6,11 +6,14 @@ interface DeckProps {
 
 function DeckDetails({ deck }: DeckProps) {
   return (
-    <div className="flex size-60 justify-center rounded-md bg-tertiary bg-[url('/deck.png')] bg-cover pt-3 shadow-xl">
-      <div className="flex h-[15%] w-full justify-center px-2">
+    <div className="flex size-60 flex-col items-center justify-between rounded-md bg-tertiary bg-[url('/deck.png')] bg-cover pt-3 shadow-xl">
+      <div className="flex h-[15%] w-full px-2">
         <h3 className="w-full break-words text-center font-patua text-xl text-textPrimary">
           {deck.name}
         </h3>
+      </div>
+      <div className="flex h-16 w-full justify-between">
+        <img src="/modification.png" alt="Modification icon" className="w-16" />
       </div>
     </div>
   );
