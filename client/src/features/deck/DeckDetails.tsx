5b908c87b190ebf332e33dd3cf3d6bfdb5afa788
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-04 20:51:10 +0200

    moved deck modification logic into it's own component & fixed cancel deck edit issue

--- client/src/features/deck/DeckDetails.tsx
+++ client/src/features/deck/DeckDetails.tsx
@@ -1,72 +1,13 @@
 import { useState } from "react";
 import { Deck } from "../../store/deck/deckSlice";
-import { useAppDispatch } from "../../store/hooks";
-import { updateDeck } from "../../store/deck/deckThunk";
+import DeckModification from "./DeckModification";
 
-interface DeckProps {
+export interface DeckProps {
   deck: Deck;
-}
-
-const initialState = {
-  name: ""
-};
-
-interface ApiErrorResponse {
-  errors: {
-    message: string;
-    field?: string;
-  }[];
 }
 
 function DeckDetails({ deck }: DeckProps) {
   const [isModifying, setIsModifying] = useState(false);
-  const [deckData, setDeckData] = useState(initialState);
-  const [error, setError] = useState({
-    name: ""
-  });
-  const dispatch = useAppDispatch();
-
-  const handleEdit = () => {
-    setDeckData({ name: deck.name });
-    setIsModifying(true);
-  };
-
-  const handleSubmit = () => async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!deckData.name) return;
-
-    try {
-      await dispatch(updateDeck({ id: deck.id, data: deckData })).unwrap();
-
-      setDeckData(initialState);
-      setIsModifying(false);
-    } catch (err: unknown) {
-      const error = err as ApiErrorResponse;
-
-      if (error.errors) {
-        for (const e of error.errors) {
-          if (e.field === "name") {
-            setError((prev) => ({ ...prev, name: e.message }));
-          }
-        }
-      }
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-
-    setError((prev) => ({
-      ...prev,
-      name: ""
-    }));
-
-    setDeckData((prev) => ({
-      ...prev,
-      name: value
-    }));
-  };
 
   return (
     <div className={`flip-box-left ${isModifying ? "flip" : ""}`}>
@@ -79,7 +20,10 @@ function DeckDetails({ deck }: DeckProps) {
               </h3>
             </div>
             <div className="flex h-16 w-full justify-between">
-              <button type="button" onClick={handleEdit}>
+              <button
+                type="button"
+                onClick={() => setIsModifying(!isModifying)}
+              >
                 <img
                   src="/modification.png"
                   alt="Modification icon"
@@ -89,48 +33,11 @@ function DeckDetails({ deck }: DeckProps) {
             </div>
           </div>
         </div>
-        <div className="flip-box-b-left size-60 rounded-lg bg-tertiary shadow-lg">
-          <div className="flex h-full flex-col justify-between">
-            <h3 className="mt-4 text-center font-patua text-xl">Modifier</h3>
-            <div className="flex h-full flex-col items-center justify-center">
-              <form
-                className="flex flex-col items-center gap-2"
-                onSubmit={handleSubmit()}
-              >
-                <input
-                  id="name"
-                  type="text"
-                  value={deckData.name}
-                  onChange={(e) => handleChange(e)}
-                  placeholder="Nom du deck"
-                  className="mt-2 h-10 w-44 rounded-lg pl-2 font-patua shadow-inner-strong placeholder:text-black/20 placeholder:text-opacity-70"
-                />
-                {error.name && (
-                  <p className="w-44 break-words font-patua text-sm text-red-500">
-                    {error.name}
-                  </p>
-                )}
-                <div className="flex w-full justify-between gap-10">
-                  <button type="button">
-                    <img
-                      src="/cancelation.png"
-                      alt="Cancelation icon"
-                      className="w-20"
-                      onClick={() => setIsModifying(!isModifying)}
-                    />
-                  </button>
-                  <button type="submit" className="mr-2">
-                    <img
-                      src="/validation.png"
-                      alt="Validation icon"
-                      className="w-16"
-                    />
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+        <DeckModification
+          deck={deck}
+          isModifying={isModifying}
+          setIsModifying={setIsModifying}
+        />
       </div>
     </div>
   );
