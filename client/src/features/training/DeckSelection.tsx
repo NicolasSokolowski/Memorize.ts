eabb437643e377cc28af0adc3c289523c5f7368c
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-26 01:18:17 +0200

    added return link in deck selection

--- client/src/features/training/DeckSelection.tsx
+++ client/src/features/training/DeckSelection.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { getDecks } from "../../store/deck/deckThunk";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import DeckPicker from "./DeckPicker";
+import { Link } from "react-router-dom";
 
 function DeckSelection() {
   const dispatch = useAppDispatch();
@@ -14,6 +15,12 @@ function DeckSelection() {
   return (
     <div className="h-full overflow-y-auto bg-primary p-12">
       <div className="grid grid-cols-[repeat(auto-fit,_15rem)] gap-12 pb-8">
+        <Link
+          to="/user/training/mode"
+          className="flex size-60 flex-col items-center justify-center rounded-md bg-tertiary shadow-xl"
+        >
+          <span className="font-patua text-9xl text-secondary">&lt;</span>
+        </Link>
         {decks.map((deck) => (
           <DeckPicker key={deck.id} deck={deck} />
         ))}
