70767c8a38abf2e837f41f8add1a350a6944273d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-29 01:00:59 +0200

    training session's scoreboard stats implementation

--- client/src/features/training/DeckTraining.tsx
+++ client/src/features/training/DeckTraining.tsx
@@ -43,8 +43,26 @@ function DeckTraining() {
     }
   }, [navigate, cards]);
 
+  const winningCards = cardsToUpdate.filter((updatedCard) => {
+    const originalCard = cards.find((c) => c.id === updatedCard.id);
+    return updatedCard.user_answer === "easy" && originalCard!.win_streak >= 1;
+  });
+
+  const winStreakCount = winningCards.length;
+
+  const total = cardsToUpdate.length;
+  const easyCount = cardsToUpdate.filter(
+    (card) => card.user_answer === "easy"
+  ).length;
+  const mediumCount = cardsToUpdate.filter(
+    (card) => card.user_answer === "medium"
+  ).length;
+
+  const successRate =
+    total > 0 ? Math.round(((easyCount + mediumCount / 2) / total) * 100) : 0;
+
   return (
-    <div className="flex min-h-screen w-full flex-col justify-center bg-primary">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-primary">
       {cardIndex < cards.length && (
         <>
           <div className="flex h-32 w-full items-center">
@@ -121,6 +139,82 @@ function DeckTraining() {
             </div>
           </div>
         </>
+      )}
+      {cardIndex >= cards.length && (
+        <div className="mx-20 flex w-3/5 flex-col rounded-lg bg-tertiary shadow-xl">
+          <span className="mt-5 p-10 pb-20 text-center font-patua text-7xl text-textPrimary">
+            Tableau des scores
+          </span>
+          <div className="my-4 flex flex-col items-center justify-center">
+            <div className="flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Nombre de cartes révisées :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(cardsToUpdate.length).padStart(2, "0")}
+              </div>
+            </div>
+            <div className="flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Nombre de cartes faciles :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(
+                  cardsToUpdate.filter((card) => card.user_answer === "easy")
+                    .length
+                ).padStart(2, "0")}
+              </div>
+            </div>
+            <div className="flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Nombre de cartes moyennes :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(
+                  cardsToUpdate.filter((card) => card.user_answer === "medium")
+                    .length
+                ).padStart(2, "0")}
+              </div>
+            </div>
+            <div className="flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Nombre de cartes difficiles :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(
+                  cardsToUpdate.filter((card) => card.user_answer === "hard")
+                    .length
+                ).padStart(2, "0")}
+              </div>
+            </div>
+            <div className="mt-10 flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Nombre de nouvelles cartes :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(
+                  cards.filter((card) => card.difficulty === 0).length
+                ).padStart(2, "0")}
+              </div>
+            </div>
+            <div className="flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Nombre de cartes en série de victoires :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(winStreakCount).padStart(2, "0")}
+              </div>
+            </div>
+            <div className="flex w-4/5 justify-between">
+              <div className="h-12 w-4/5 font-patua text-4xl text-textPrimary">
+                Taux de réussite :
+              </div>
+              <div className="h-12 font-patua text-4xl text-textPrimary">
+                {String(successRate).padStart(2, "0")}%
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
