afe69bc712f97f0c39620473d6a1b4554f049871
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-28 19:18:29 +0200

    handleFlip function implementation

--- client/src/features/training/DeckTraining.tsx
+++ client/src/features/training/DeckTraining.tsx
@@ -5,11 +5,17 @@ import { useNavigate, useParams } from "react-router-dom";
 function DeckTraining() {
   const { deckId } = useParams<{ deckId: string }>();
   const [isFlipped, setIsFlipped] = useState(false);
+  const [flipCount, setFlipCount] = useState(0);
   const [cardIndex, setCardIndex] = useState(0);
   const navigate = useNavigate();
   const cards = useAppSelector((state) =>
     state.card.cards.filter((card) => card.deck_id === parseInt(deckId!, 10))
   );
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+    setFlipCount(flipCount + 1);
+  };
 
   useEffect(() => {
     if (cards.length === 0) {
@@ -25,7 +31,7 @@ function DeckTraining() {
             <div className="flip-box-inner">
               <div
                 className="flip-training-a flex h-112 w-112 flex-col rounded-lg bg-tertiary bg-[url('/card.png')] bg-[length:60%] bg-center bg-no-repeat shadow-xl"
-                onClick={() => setIsFlipped(!isFlipped)}
+                onClick={() => handleFlip()}
               >
                 <span className="mt-8 flex w-full justify-center font-patua text-5xl text-textPrimary">
                   {cards[cardIndex].front}
@@ -33,7 +39,7 @@ function DeckTraining() {
               </div>
               <div
                 className="flip-training-b flex h-112 w-112 flex-col rounded-lg bg-tertiary bg-[url('/cardback.png')] bg-[length:60%] bg-center bg-no-repeat shadow-xl"
-                onClick={() => setIsFlipped(!isFlipped)}
+                onClick={() => handleFlip()}
               >
                 <span className="mt-8 flex w-full justify-center font-patua text-5xl text-textPrimary">
                   {cards[cardIndex].back}
@@ -41,23 +47,25 @@ function DeckTraining() {
               </div>
             </div>
           </div>
-          <div className="mt-10 flex h-44 w-full justify-center gap-5">
-            <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
-              <div className="relative size-36 rounded-full bg-green-500 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
-                <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
-              </div>
-            </button>
-            <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
-              <div className="relative size-36 rounded-full bg-orange-400 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
-                <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
-              </div>
-            </button>
-            <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
-              <div className="relative size-36 rounded-full bg-red-600 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
-                <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
-              </div>
-            </button>
-          </div>
+          {flipCount > 0 && (
+            <div className="mt-10 flex h-44 w-full justify-center gap-5">
+              <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
+                <div className="relative size-36 rounded-full bg-green-500 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
+                  <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
+                </div>
+              </button>
+              <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
+                <div className="relative size-36 rounded-full bg-orange-400 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
+                  <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
+                </div>
+              </button>
+              <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
+                <div className="relative size-36 rounded-full bg-red-600 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
+                  <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
+                </div>
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
