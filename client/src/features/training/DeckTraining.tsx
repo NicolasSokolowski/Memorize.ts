a1b553815b342fb4c706fd28373fe3a11c1d40aa
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-28 20:46:57 +0200

    added disabled property to button with color changes

--- client/src/features/training/DeckTraining.tsx
+++ client/src/features/training/DeckTraining.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { useEffect, useState } from "react";
 import { useAppSelector } from "../../store/hooks";
 import { useNavigate, useParams } from "react-router-dom";
@@ -45,8 +46,10 @@ function DeckTraining() {
 
   return (
     <div className="flex min-h-screen w-full flex-col justify-center bg-primary">
+      {/* <div className="flex bg-red-500 w-full h-12 ">
+      </div> */}
       {cardIndex < cards.length && (
-        <div className="flex h-full flex-col items-center justify-center">
+        <div className="flex h-full flex-col items-center justify-center self-center">
           <div className={`flip-training ${isFlipped ? "flip" : ""}`}>
             <div className="flip-box-inner">
               <div
@@ -67,37 +70,44 @@ function DeckTraining() {
               </div>
             </div>
           </div>
-          {flipCount > 0 && (
-            <div className="mt-10 flex h-44 w-full justify-center gap-5">
-              <button
-                className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
-                value="easy"
-                onClick={(e) => handleNextCard(e)}
+          <div className="mt-10 flex h-44 w-full justify-center gap-5">
+            <button
+              className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
+              value="easy"
+              onClick={(e) => handleNextCard(e)}
+              disabled={flipCount === 0}
+            >
+              <div
+                className={`relative size-36 rounded-full shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]  disabled:cursor-not-allowed ${flipCount === 0 ? "bg-gray-300" : "bg-green-500"}`}
               >
-                <div className="relative size-36 rounded-full bg-green-500 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
-                  <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
-                </div>
-              </button>
-              <button
-                className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
-                value="medium"
-                onClick={(e) => handleNextCard(e)}
+                <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
+              </div>
+            </button>
+            <button
+              className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
+              value="medium"
+              onClick={(e) => handleNextCard(e)}
+              disabled={flipCount === 0}
+            >
+              <div
+                className={`relative size-36 rounded-full shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)] disabled:cursor-not-allowed ${flipCount === 0 ? "bg-gray-300" : "bg-orange-400"}`}
               >
-                <div className="relative size-36 rounded-full bg-orange-400 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
-                  <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
-                </div>
-              </button>
-              <button
-                className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
-                value="hard"
-                onClick={(e) => handleNextCard(e)}
+                <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
+              </div>
+            </button>
+            <button
+              className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
+              value="hard"
+              onClick={(e) => handleNextCard(e)}
+              disabled={flipCount === 0}
+            >
+              <div
+                className={`relative size-36 rounded-full shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)] disabled:cursor-not-allowed ${flipCount === 0 ? "bg-gray-300" : "bg-red-600"}`}
               >
-                <div className="relative size-36 rounded-full bg-red-600 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
-                  <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
-                </div>
-              </button>
-            </div>
-          )}
+                <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
+              </div>
+            </button>
+          </div>
         </div>
       )}
     </div>
