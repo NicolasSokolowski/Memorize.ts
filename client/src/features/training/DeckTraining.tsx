0be4a914cdd576f9124c26e536e4122d49db8f3d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-28 20:15:35 +0200

    handle next card function implementation

--- client/src/features/training/DeckTraining.tsx
+++ client/src/features/training/DeckTraining.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { useAppSelector } from "../../store/hooks";
 import { useNavigate, useParams } from "react-router-dom";
 
+type UserAnswer = {
+  id: number;
+  user_answer: string;
+};
+
 function DeckTraining() {
   const { deckId } = useParams<{ deckId: string }>();
   const [isFlipped, setIsFlipped] = useState(false);
@@ -11,10 +16,25 @@ function DeckTraining() {
   const cards = useAppSelector((state) =>
     state.card.cards.filter((card) => card.deck_id === parseInt(deckId!, 10))
   );
+  const [cardsToUpdate, setCardsToUpdate] = useState<UserAnswer[]>([]);
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
     setFlipCount(flipCount + 1);
+  };
+
+  const handleNextCard = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const button = e.currentTarget as HTMLButtonElement;
+    const value = button.value;
+    setCardsToUpdate((prev) => [
+      ...prev,
+      { id: cards[cardIndex].id, user_answer: value }
+    ]);
+    setFlipCount(0);
+    setIsFlipped(false);
+    setTimeout(() => {
+      setCardIndex((prev) => prev + 1);
+    }, 200);
   };
 
   useEffect(() => {
@@ -49,17 +69,29 @@ function DeckTraining() {
           </div>
           {flipCount > 0 && (
             <div className="mt-10 flex h-44 w-full justify-center gap-5">
-              <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
+              <button
+                className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
+                value="easy"
+                onClick={(e) => handleNextCard(e)}
+              >
                 <div className="relative size-36 rounded-full bg-green-500 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
                   <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
                 </div>
               </button>
-              <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
+              <button
+                className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
+                value="medium"
+                onClick={(e) => handleNextCard(e)}
+              >
                 <div className="relative size-36 rounded-full bg-orange-400 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
                   <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
                 </div>
               </button>
-              <button className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong">
+              <button
+                className="flex size-44 items-center justify-center rounded-full bg-tertiary shadow-inner-strong"
+                value="hard"
+                onClick={(e) => handleNextCard(e)}
+              >
                 <div className="relative size-36 rounded-full bg-red-600 shadow-[0_3px_8px_rgba(0,0,0,0.3),inset_0_2px_3px_rgba(0,0,0,0.1)]">
                   <div className="absolute -inset-4 -z-10 rounded-full border-t-2"></div>
                 </div>
