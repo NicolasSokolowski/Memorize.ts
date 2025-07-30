43bc341808807d7d48fd849843ef7500a124e8e1
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-07-30 13:40:04 +0200

    moved onClick deck training to the right div

--- client/src/features/training/DeckModeSelection.tsx
+++ client/src/features/training/DeckModeSelection.tsx
@@ -14,15 +14,15 @@ function DeckModeSelection() {
   }, [dispatch]);
 
   return (
-    <div
-      className="flex size-full flex-col"
-      onClick={() => navigate("/user/training/decks")}
-    >
+    <div className="flex size-full flex-col">
       <h1 className="flex h-1/3 items-center justify-center font-patua text-8xl text-textPrimary">
         Sélectionnez un deck
       </h1>
       <div className="flex h-80 w-full justify-center">
-        <div className="flex size-80 flex-col justify-between rounded-lg bg-tertiary bg-[url('/deck.png')] bg-cover shadow-xl">
+        <div
+          className="flex size-80 flex-col justify-between rounded-lg bg-tertiary bg-[url('/deck.png')] bg-cover shadow-xl"
+          onClick={() => navigate("/user/training/decks")}
+        >
           <div className="flex h-16 w-full items-center justify-center font-patua text-2xl text-textPrimary">
             Choisir un deck
           </div>
