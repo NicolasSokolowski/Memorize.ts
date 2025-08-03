6c1c0d4c733b6b34e7f521e633b1df1dc2b9fd14
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-03 11:52:30 +0200

    User's profile page colors implementation

--- client/src/features/user/UserProfile.tsx
+++ client/src/features/user/UserProfile.tsx
@@ -1,38 +1,38 @@
 function UserProfile() {
   return (
-    <div className="h-full bg-red-500">
-      <div className="flex h-80 justify-center bg-blue-500">
-        <div className="mt-5 flex size-80 items-center justify-center rounded-full bg-yellow-500 shadow-xl">
-          <img className="size-64 rounded-full bg-green-500 shadow-inner-strong" />
+    <div className="h-full">
+      <div className="flex h-96 items-center justify-center">
+        <div className="flex size-80 items-center justify-center rounded-full bg-tertiary shadow-xl">
+          <img className="size-64 rounded-full bg-white shadow-inner-strong" />
         </div>
       </div>
-      <div className="mx-20 mt-5 h-32 bg-orange-500">
-        <div className="h-16 bg-violet-500">
-          <div className="ml-52 h-full w-112 bg-white"></div>
+      <div className="mx-20 mt-5 h-32">
+        <div className="h-16">
+          <div className="ml-52 h-full w-112"></div>
         </div>
-        <div className="h-16 bg-cyan-500">
-          <div className="ml-52 h-full w-112 bg-black"></div>
+        <div className="h-16">
+          <div className="ml-52 h-full w-112"></div>
         </div>
       </div>
-      <div className="mx-20 mt-5 flex h-2/5 justify-center gap-32 bg-gray-500">
-        <div className="m-5 flex w-112 flex-col gap-4 bg-fuchsia-500 ">
-          <button className="h-16 w-full rounded-md bg-emerald-500 shadow-md">
+      <div className="mx-20 mt-5 flex h-2/5 justify-center gap-32">
+        <div className="m-5 flex w-112 flex-col gap-4">
+          <button className="h-16 w-full rounded-md bg-secondary shadow-md">
             Button 1
           </button>
-          <button className="h-16 w-full rounded-md bg-emerald-500 shadow-md">
+          <button className="h-16 w-full rounded-md bg-secondary shadow-md">
             Button 2
           </button>
-          <button className="h-16 w-full rounded-md bg-emerald-500 shadow-md">
+          <button className="h-16 w-full rounded-md bg-secondary shadow-md">
             Button 3
           </button>
-          <button className="h-16 w-full rounded-md bg-emerald-500 shadow-md">
+          <button className="h-16 w-full rounded-md bg-secondary shadow-md">
             Button 4
           </button>
-          <button className="h-16 w-full rounded-md bg-emerald-500 shadow-md">
+          <button className="h-16 w-full rounded-md bg-secondary shadow-md">
             Button 5
           </button>
         </div>
-        <div className="m-5 w-1/4 rounded-lg bg-lime-300 shadow-lg"></div>
+        <div className="m-5 w-1/4 rounded-lg bg-tertiary shadow-lg"></div>
       </div>
     </div>
   );
