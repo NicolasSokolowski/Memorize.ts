63f0fc6097852eba66d25eb87e9882f5a2ae6d7b
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-08 00:30:15 +0200

    Email Service class implementation

---
+++ server/src/services/EmailService.ts
@@ -0,0 +1,54 @@
+import nodemailer, { Transporter } from "nodemailer";
+import ejs from "ejs";
+import path from "path";
+import { readFile } from "fs/promises";
+
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env;
+
+type EmailData = {
+  to: string;
+  subject: string;
+  template: string;
+  context: Record<string, string>;
+};
+
+export class EmailService {
+  private transporter: Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: SMTP_HOST,
+      port: Number(SMTP_PORT),
+      auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD
+      }
+    });
+  }
+
+  private async renderTemplate(
+    template: string,
+    context: Record<string, string>
+  ) {
+    const filePath = path.join(
+      __dirname,
+      "..",
+      "templates",
+      "SMTPs",
+      `${template}.ejs`
+    );
+    const content = await readFile(filePath, "utf-8");
+    return ejs.render(content, context);
+  }
+
+  async sendEmail({ to, subject, template, context }: EmailData) {
+    const html = await this.renderTemplate(template, context);
+
+    await this.transporter.sendMail({
+      from: `"Memorize" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+  }
+}
