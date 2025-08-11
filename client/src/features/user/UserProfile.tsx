8bbb5b59dcb1978643a1cffe582d48045b6e7dd8
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-11 21:48:43 +0200

    modified button to display email modification form

--- client/src/features/user/UserProfile.tsx
+++ client/src/features/user/UserProfile.tsx
@@ -5,9 +5,15 @@ import UsernameForm from "./UsernameForm";
 import PasswordForm from "./PasswordForm";
 import LogoutForm from "./LogoutForm";
 import DeleteAccount from "./DeleteAccount";
+import EmailForm from "./EmailForm";
 
 type UserModification = "none" | EditActions;
-type EditActions = "edit-username" | "edit-password" | "logout" | "delete-user";
+type EditActions =
+  | "edit-username"
+  | "edit-email"
+  | "edit-password"
+  | "logout"
+  | "delete-user";
 
 function UserProfile() {
   const [visibleForm, setVisibleForm] = useState<UserModification>("none");
@@ -69,8 +75,14 @@ function UserProfile() {
               Modifier mon nom d'utilisateur
             </span>
           </button>
-          <button className="h-16 w-full rounded-md bg-secondary shadow-md">
-            Button 2
+          <button
+            className={`h-16 w-full rounded-md shadow-md ${visibleForm === "edit-email" ? "bg-tertiary text-textPrimary" : "bg-secondary text-white"}`}
+            value="edit-email"
+            onClick={(e) => handleEdit(e)}
+          >
+            <span className="font-patua text-xl">
+              Modifier mon adresse e-mail
+            </span>
           </button>
           <button
             className={`h-16 w-full rounded-md shadow-md ${visibleForm === "edit-password" ? "bg-tertiary text-textPrimary" : "bg-secondary text-white"}`}
@@ -102,6 +114,14 @@ function UserProfile() {
             <div className="flip-box-profile-edit">
               {visibleForm === "edit-username" && (
                 <UsernameForm
+                  onCancel={() => {
+                    setIsEditing(false);
+                    setTimeout(() => setVisibleForm("none"), 800);
+                  }}
+                />
+              )}
+              {visibleForm === "edit-email" && (
+                <EmailForm
                   onCancel={() => {
                     setIsEditing(false);
                     setTimeout(() => setVisibleForm("none"), 800);
