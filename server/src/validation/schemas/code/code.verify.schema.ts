561b28ba783f78af722f2e05dde4212ae990e2ac
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-14 14:18:10 +0200

    modified verification code schema validation

--- server/src/validation/schemas/code/code.verify.schema.ts
+++ server/src/validation/schemas/code/code.verify.schema.ts
@@ -4,13 +4,15 @@ export const verifyCodeSchema = Joi.object({
   requestType: Joi.string().max(50).required(),
   code: Joi.string().length(4).required(),
 
-  newEmail: Joi.when("requestType", {
+  data: Joi.when("requestType", {
     is: "EMAIL_CHANGE",
-    then: Joi.string().email().required().messages({
-      "string.base": "Email must be a string",
-      "string.email": "Email must be a valid email address",
-      "string.empty": "Email cannot be empty"
-    }),
+    then: Joi.object({
+      newEmail: Joi.string().email().required().messages({
+        "string.base": "Email must be a string",
+        "string.email": "Email must be a valid email address",
+        "string.empty": "Email cannot be empty"
+      })
+    }).required(),
     otherwise: Joi.forbidden()
   })
 });
