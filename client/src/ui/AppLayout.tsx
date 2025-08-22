00e5691ead8625c1c03b62ca0b6979a3b0168cd9
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-22 19:59:57 +0200

    footer implementation

--- client/src/ui/AppLayout.tsx
+++ client/src/ui/AppLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "../flip.css";
+import Footer from "./Footer";
 
 function AppLayout() {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="relative h-screen flex-col">
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
