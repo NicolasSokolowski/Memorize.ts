6443712b3e50fda60f1036bf8bff22cb6d916ac1
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-22 13:58:46 +0200

    Displaying current deck

--- client/src/ui/SearchBarLayout.tsx
+++ client/src/ui/SearchBarLayout.tsx
@@ -11,7 +11,13 @@ function SearchBarLayout() {
   const [searchedItem, setSearchedItem] = useState("");
   const location = useLocation();
 
-  const store = location.pathname.includes("/cards")
+  const cardsLocation = location.pathname.includes("/cards");
+
+  const deck = useAppSelector((state) =>
+    state.deck.decks.find((deck) => deck.id === deckIdNumber)
+  );
+
+  const store = cardsLocation
     ? { slice: "card" as const, items: "cards" as const }
     : { slice: "deck" as const, items: "decks" as const };
 
@@ -41,8 +47,19 @@ function SearchBarLayout() {
 
   return (
     <>
-      <div className="relative flex h-14 justify-center bg-white shadow-bottom">
-        <div className="w-1/4">
+      <div className="relative flex h-14 bg-white shadow-bottom">
+        {cardsLocation && (
+          <div className="relative ml-12 flex w-1/6 items-center justify-center">
+            <div className="flex h-10 w-full items-center rounded-full bg-primary shadow-inner-strong">
+              {deck && (
+                <span className="ml-5 font-patua text-lg text-textPrimary">
+                  {deck.name}
+                </span>
+              )}
+            </div>
+          </div>
+        )}
+        <div className="absolute left-1/2 top-1/2 w-1/4 -translate-x-1/2 -translate-y-1/2">
           <div className="my-2 flex h-10 w-full items-center justify-between rounded-full bg-tertiary shadow-inner-strong">
             <input
               className="ml-5 bg-transparent font-patua text-textPrimary focus:outline-none"
