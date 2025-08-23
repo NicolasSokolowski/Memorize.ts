49faff3a8372d1b9acb5acf104172180b33e88c7
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-23 12:09:12 +0200

    Sorting decks by number of cards (desc), then by alphabetic order

--- client/src/features/deck/DecksList.tsx
+++ client/src/features/deck/DecksList.tsx
@@ -14,7 +14,21 @@ function DecksList() {
     (state) => state.deck.hasBeenFetchedOnce
   );
 
+  const cards = useAppSelector((state) => state.card.cards);
+
   const filteredItems = useOutletContext<Deck[]>() || decks;
+
+  const sortedDecks = [...filteredItems].sort((a, b) => {
+    const aCount = cards.filter((c) => c.deck_id === a.id).length;
+    const bCount = cards.filter((c) => c.deck_id === b.id).length;
+
+    if (aCount > 0 && bCount === 0) return -1;
+    if (aCount === 0 && bCount > 0) return 1;
+
+    if (aCount !== bCount) return bCount - aCount;
+
+    return a.name.localeCompare(b.name, "fr", { sensitivity: "base" });
+  });
 
   useEffect(() => {
     if (!hasBeenFetchedOnce) {
@@ -27,7 +41,7 @@ function DecksList() {
     <div className="scrollbar-hide h-full overflow-y-auto bg-primary p-12">
       <div className="grid grid-cols-[repeat(auto-fit,_15rem)] gap-12 pb-8">
         <DeckCreation />
-        {filteredItems.map((deck) => (
+        {sortedDecks.map((deck) => (
           <DeckDetails key={deck.id} deck={deck} />
         ))}
       </div>
