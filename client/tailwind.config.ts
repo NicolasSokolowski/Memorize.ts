0374843a42495945dc2dd741b9e4080f02ba5348
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-08-27 01:45:02 +0200

    adding tailwind breakpoint for smaller resolution (xs)

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "575px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px"
+    },
     fontFamily: {
       patua: ['"Patua One"', "cursive"]
     },
