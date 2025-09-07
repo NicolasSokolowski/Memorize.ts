4447fdc55cd00b974947d0e2b2f2259a91cf8cab
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-08 00:55:56 +0200

    modified messages to lowercase

--- server/src/validation/schemas/user/user.create.schema.ts
+++ server/src/validation/schemas/user/user.create.schema.ts
@@ -2,19 +2,19 @@ import Joi from "../../../helpers/passwordComplexity.helper";
 
 export default Joi.object({
   email: Joi.string().email().required().messages({
-    "string.base": "Email must be a string",
-    "string.email": "Email must be a valid email address",
-    "string.empty": "Email cannot be empty"
+    "string.base": "email must be a string",
+    "string.email": "email must be a valid email address",
+    "string.empty": "email cannot be empty"
   }),
   password: Joi.string().min(12).passwordComplexity().required().messages({
-    "string.base": "Password must be a string",
-    "string.empty": "Password cannot be empty",
-    "string.min": "Password must be at least 12 characters long"
+    "string.base": "password must be a string",
+    "string.empty": "password cannot be empty",
+    "string.min": "password must be at least 12 characters long"
   }),
   username: Joi.string().min(3).max(20).required().messages({
-    "string.base": "Username must be a string",
-    "string.empty": "Username cannot be empty",
-    "string.min": "Username must be at least 3 characters long",
-    "string.max": "Username must be at most 20 characters long"
+    "string.base": "username must be a string",
+    "string.empty": "username cannot be empty",
+    "string.min": "username must be at least 3 characters long",
+    "string.max": "username must be at most 20 characters long"
   })
 });
