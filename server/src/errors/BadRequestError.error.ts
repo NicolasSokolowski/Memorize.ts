aa827775b92fbd6dabffb83cc417cd48c2edda3b
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-08 00:55:28 +0200

    BadRequestError now accepts field optionally

--- server/src/errors/BadRequestError.error.ts
+++ server/src/errors/BadRequestError.error.ts
@@ -3,15 +3,17 @@ import { CustomError } from "./CustomError.error";
 export class BadRequestError extends CustomError {
   statusCode = 400;
   code: string;
+  field?: string;
 
-  constructor(message: string, code: string) {
+  constructor(message: string, code: string, field?: string) {
     super(message);
     this.code = code;
+    this.field = field;
 
     Object.setPrototypeOf(this, BadRequestError.prototype);
   }
 
   serializeErrors() {
-    return [{ message: this.message, code: this.code }];
+    return [{ message: this.message, code: this.code, field: this.field }];
   }
 }
