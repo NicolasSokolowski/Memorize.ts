f9185368d944dbfcf50b5419f6c2ffcd9d3d7256
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-09 01:10:33 +0200

    Password reset's new password form now displays error messages

--- client/src/features/user/PasswordResetForm.tsx
+++ client/src/features/user/PasswordResetForm.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../../services/axios.instance";
-import axios from "axios";
-import { onCancelProp } from "../../types/user";
+import { AxiosError } from "axios";
+import { errorInitialState, onCancelProp } from "../../types/user";
+import Error from "../../ui/Error";
+import { ApiErrorResponse } from "../../types/api";
 
 function PasswordResetForm({ onCancel }: onCancelProp) {
   const [passwordHasBeenChanged, setPasswordHasBeenChanged] = useState(false);
@@ -9,7 +11,7 @@ function PasswordResetForm({ onCancel }: onCancelProp) {
     newPassword: "",
     passwordConfirmation: ""
   });
-  const [error, setError] = useState("");
+  const [error, setError] = useState(errorInitialState);
 
   useEffect(() => {
     if (passwordHasBeenChanged) {
@@ -22,7 +24,7 @@ function PasswordResetForm({ onCancel }: onCancelProp) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
 
-    setError("");
+    setError(errorInitialState);
 
     setNewPassword((prev) => ({
       ...prev,
@@ -34,90 +36,109 @@ function PasswordResetForm({ onCancel }: onCancelProp) {
     e.preventDefault();
 
     if (!newPassword.newPassword || !newPassword.passwordConfirmation) {
-      setError("Tous les champs sont requis.");
-    } else if (newPassword.newPassword !== newPassword.passwordConfirmation) {
-      setError("Les mots de passe sont différent");
+      setError({
+        ...error,
+        messages: [...error.messages, "Tous les champs sont requis"]
+      });
+      return;
+    }
+
+    if (newPassword.newPassword !== newPassword.passwordConfirmation) {
+      setError({
+        ...error,
+        messages: [...error.messages, "Les mots de passe sont différents"]
+      });
+      return;
     }
 
     try {
       await axiosInstance.patch("/profile/resetpw", newPassword);
       setPasswordHasBeenChanged(true);
     } catch (err: unknown) {
-      if (axios.isAxiosError(err) && err.response?.data?.errors) {
-        setError(err.response.data.errors[0].message);
+      const axiosError = err as AxiosError<ApiErrorResponse>;
+
+      if (axiosError.response?.data.errors) {
+        for (const apiError of axiosError.response.data.errors) {
+          setError((prev) => ({
+            ...prev,
+            fields: apiError.field
+              ? [...new Set([...prev.fields, apiError.field])]
+              : prev.fields,
+            messages: apiError.message
+              ? [...prev.messages, apiError.message]
+              : prev.messages
+          }));
+        }
       }
     }
   };
 
   return (
-    <form
-      className="mt-8 flex flex-1 flex-col justify-center"
-      onSubmit={handleSubmit}
-    >
-      <label
-        htmlFor="new-pw"
-        className="ml-1 font-patua text-xl text-textPrimary"
+    <>
+      <form
+        className="mt-8 flex flex-1 flex-col justify-center"
+        onSubmit={handleSubmit}
       >
-        Nouveau mot de passe
-      </label>
-      <input
-        id="newPassword"
-        type="password"
-        className="my-2 h-10 rounded-lg pl-3 font-patua text-lg text-textPrimary shadow-inner-strong"
-        value={newPassword.newPassword}
-        onChange={(e) => handleChange(e)}
-        autoComplete="off"
-      />
-      <label
-        htmlFor="passwordConfirmation"
-        className="ml-1 font-patua text-xl text-textPrimary"
-      >
-        Confirmation mot de passe
-      </label>
-      <input
-        id="passwordConfirmation"
-        type="password"
-        className="my-2 h-10 rounded-lg pl-3 font-patua text-lg text-textPrimary shadow-inner-strong"
-        value={newPassword.passwordConfirmation}
-        onChange={(e) => handleChange(e)}
-        autoComplete="off"
-      />
-      {error ? (
-        <p className="break-words pl-2 font-patua text-red-500">{error}</p>
-      ) : (
-        <div className="">
-          {!passwordHasBeenChanged ? (
-            <div className="mt-5 flex h-24 w-full justify-center gap-20">
-              <button type="button">
-                <img
-                  src="/cancelation.png"
-                  alt="Cancelation icon"
-                  onClick={onCancel}
-                  className="w-24"
-                  draggable={false}
-                />
-              </button>
-              <button type="submit" className="mr-2">
-                <img
-                  src="/validation.png"
-                  alt="Validation icon"
-                  className="w-20"
-                  draggable={false}
-                />
-              </button>
-            </div>
-          ) : (
-            <div className="mt-5 flex h-24 flex-col items-center justify-end">
-              <p className="mt-4 text-center font-patua text-base text-textPrimary">
-                Succès : Un e-mail de confirmation vous a été envoyé. Vous
-                pouvez dès à pouvez dès à présent vous connecter avec votre
-                nouveau mot de passe.
-              </p>
-            </div>
-          )}
-        </div>
-      )}
-    </form>
+        <label
+          htmlFor="new-pw"
+          className="ml-1 font-patua text-xl text-textPrimary"
+        >
+          Nouveau mot de passe
+        </label>
+        <input
+          id="newPassword"
+          type="password"
+          className="my-2 h-10 rounded-lg pl-3 font-patua text-lg text-textPrimary shadow-inner-strong"
+          value={newPassword.newPassword}
+          onChange={(e) => handleChange(e)}
+          autoComplete="off"
+        />
+        <label
+          htmlFor="passwordConfirmation"
+          className="ml-1 font-patua text-xl text-textPrimary"
+        >
+          Confirmation mot de passe
+        </label>
+        <input
+          id="passwordConfirmation"
+          type="password"
+          className="my-2 h-10 rounded-lg pl-3 font-patua text-lg text-textPrimary shadow-inner-strong"
+          value={newPassword.passwordConfirmation}
+          onChange={(e) => handleChange(e)}
+          autoComplete="off"
+        />
+        {!passwordHasBeenChanged ? (
+          <div className="mt-5 flex h-24 w-full justify-center gap-20">
+            <button type="button">
+              <img
+                src="/cancelation.png"
+                alt="Cancelation icon"
+                onClick={onCancel}
+                className="w-24"
+                draggable={false}
+              />
+            </button>
+            <button type="submit" className="mr-2">
+              <img
+                src="/validation.png"
+                alt="Validation icon"
+                className="w-20"
+                draggable={false}
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="mt-5 flex h-24 flex-col items-center justify-end">
+            <p className="mt-4 text-center font-patua text-base text-textPrimary">
+              Succès : Un e-mail de confirmation vous a été envoyé. Vous pouvez
+              pouvez dès à pouvez dès à présent vous connecter avec votre
+              nouveau mot de passe.
+            </p>
+          </div>
+        )}
+      </form>
+      {error.messages.length > 0 && <Error error={error} />}
+    </>
   );
 }
 
