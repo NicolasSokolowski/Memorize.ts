eda3fa234ed3d4a5af4ccd2572b0213d6d10fd08
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-08 23:03:54 +0200

    joi's validation error messages are now in lower case

--- server/src/validation/schemas/user/user.signin.post.schema.ts
+++ server/src/validation/schemas/user/user.signin.post.schema.ts
@@ -2,11 +2,11 @@ import Joi from "joi";
 
 export default Joi.object({
   email: Joi.string().email().required().messages({
-    "string.base": "Email must be a string",
-    "string.email": "Email must be a valid email address",
-    "string.empty": "Email cannot be empty"
+    "string.base": "email must be a string",
+    "string.email": "email must be a valid email address",
+    "string.empty": "email cannot be empty"
   }),
   password: Joi.string().required().messages({
-    "string.base": "Password must be a string"
+    "string.base": "password must be a string"
   })
 });
