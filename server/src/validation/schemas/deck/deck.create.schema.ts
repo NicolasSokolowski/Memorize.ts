d04c5c897a69cd742cab542bfb50523868a03adb
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-09 21:11:47 +0200

    deck creation validation error message are now lowerase to fit front needs

--- server/src/validation/schemas/deck/deck.create.schema.ts
+++ server/src/validation/schemas/deck/deck.create.schema.ts
@@ -2,8 +2,8 @@ import Joi from "joi";
 
 export default Joi.object({
   name: Joi.string().max(50).required().messages({
-    "string.base": "Name must be a string",
-    "string.empty": "Name cannot be empty",
-    "string.max": "Name must be at most 50 characters long"
+    "string.base": "name must be a string",
+    "string.empty": "name cannot be empty",
+    "string.max": "name must be at most 50 characters long"
   })
 });
