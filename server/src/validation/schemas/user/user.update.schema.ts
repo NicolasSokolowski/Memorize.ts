ee7863722effe46b78c1f67fded0fd08d89086bd
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-11 00:30:22 +0200

    modified error messages to lowercase

--- server/src/validation/schemas/user/user.update.schema.ts
+++ server/src/validation/schemas/user/user.update.schema.ts
@@ -2,15 +2,15 @@ import Joi from "joi";
 
 export default Joi.object({
   email: Joi.string().email().messages({
-    "string.base": "Email must be a string",
-    "string.email": "Email must be a valid email address",
-    "string.empty": "Email cannot be empty"
+    "string.base": "email must be a string",
+    "string.email": "email must be a valid email address",
+    "string.empty": "email cannot be empty"
   }),
   username: Joi.string().min(3).max(20).messages({
-    "string.base": "Username must be a string",
-    "string.empty": "Username cannot be empty",
-    "string.min": "Username must be at least 3 characters long",
-    "string.max": "Username must be at most 20 characters long"
+    "string.base": "username must be a string",
+    "string.empty": "username cannot be empty",
+    "string.min": "username must be at least 3 characters long",
+    "string.max": "username must be at most 20 characters long"
   })
 })
   .min(1)
