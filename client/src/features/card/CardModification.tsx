e17312d9419443bfae89cceac3b9d02c7fe1717b
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-10 22:44:34 +0200

    modifying default and error input ring color

--- client/src/features/card/CardModification.tsx
+++ client/src/features/card/CardModification.tsx
@@ -129,7 +129,12 @@ function CardModification({
                 onChange={handleChange}
                 autoComplete="off"
                 placeholder={side === "front" ? "Face avant" : "Face arrière"}
-                className="mt-2 h-14 w-60 rounded-lg pl-4 shadow-inner-strong placeholder:text-black/20 placeholder:text-opacity-70 xs:h-10 xs:w-44 xs:pl-2 sm:mt-1"
+                className={`${
+                  error.fields?.includes("front") ||
+                  error.fields?.includes("back")
+                    ? "ring-2 ring-error"
+                    : ""
+                } mt-2 h-14 w-60 rounded-lg pl-4 shadow-inner-strong placeholder:text-black/20 placeholder:text-opacity-70 focus:outline-none focus:ring-2 focus:ring-primary xs:h-10 xs:w-44 xs:pl-2 sm:mt-1`}
               />
             </div>
             <div className="h-20">
