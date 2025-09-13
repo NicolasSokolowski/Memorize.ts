b3e34f445b80eb2147970ef6f2a30110d0e7fb2e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-13 13:06:48 +0200

    moved auth translation to it's own namespace and generated new types

--- client/src/@types/resources.d.ts
+++ client/src/@types/resources.d.ts
@@ -1,18 +1,23 @@
 interface Resources {
-  home: {
-    auth: {
-      buttons: {
-        signin: "Sign in";
-        signup: "Sign up";
-      };
-      connection: "Connection";
-      forgottenPassword: "Forgot password ?";
-      hasAccount: "I already have an account";
-      hasNoAccount: "I don't have an account";
-      password: "Password";
-      register: "Register";
-      username: "Username";
+  auth: {
+    buttons: {
+      signin: "Sign in";
+      signup: "Sign up";
     };
+    code: "Please enter the code you received on your e-mail :";
+    connection: "Connection";
+    forgottenPassword: "Forgot password ?";
+    hasAccount: "I already have an account";
+    hasNoAccount: "I don't have an account";
+    "new password": "New password";
+    password: "Password";
+    "password confirmation": "Password confirmation";
+    "password reset": "Password reset";
+    register: "Register";
+    "reset success": "Success : A confirmation e-mail has been sent to you. You can already sign in with your new password.";
+    username: "Username";
+  };
+  home: {
     hero: {
       subtitle: "Study smarter. Remember longer. Build your own flashcard decks in seconds and practice at your own pace. Each session challenges you to recall what’s on the back of a card—then our smart system adapts the difficulty automatically. Quickly see what you’ve mastered and what still needs work, so you can focus on what matters most. Designed to make your progress clear, effective, and lasting—whether you’re preparing for exams, learning a new language, or mastering knowledge for life.";
     };
