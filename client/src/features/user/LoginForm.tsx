a54b1ce05ef96ef6e8b1b2bd30e0d6c9a14e890d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-13 12:46:32 +0200

    deleted signin form's inputs placeholders

--- client/src/features/user/LoginForm.tsx
+++ client/src/features/user/LoginForm.tsx
@@ -122,7 +122,6 @@ function LoginForm() {
                 type="text"
                 value={userInfo.email}
                 onChange={(e) => handleChange(e)}
-                placeholder="Adresse e-mail"
                 className={`${error.messages[0]?.includes("email") ? "ring-2 ring-error" : ""} h-12 w-72 rounded-md border-gray-300 bg-white p-2 pl-3 font-patua text-textPrimary shadow-inner-strong placeholder:text-black/20 placeholder:text-opacity-70 focus:outline-none focus:ring-2 focus:ring-primary xl:w-80`}
               />
             </div>
@@ -138,7 +137,6 @@ function LoginForm() {
                 type="password"
                 value={userInfo.password}
                 onChange={(e) => handleChange(e)}
-                placeholder="Mot de passe"
                 className={`${error.messages[0]?.includes("password") ? "ring-2 ring-error" : ""} h-12 w-72 rounded-md border-gray-300 bg-white p-2 pl-3 font-patua text-black shadow-inner-strong placeholder:text-black/20 placeholder:text-opacity-70 focus:outline-none focus:ring-2 focus:ring-primary xl:w-80`}
               />
             </div>
