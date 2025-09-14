8b768503afd2f263141b0219ff1106be2b18956a
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-14 12:56:53 +0200

    calling handleApiError in handleSubmit's catch block

--- client/src/features/user/LoginForm.tsx
+++ client/src/features/user/LoginForm.tsx
@@ -7,6 +7,7 @@ import { ApiErrorResponse } from "../../types/api";
 import { errorInitialState } from "../../types/user";
 import Error from "../../ui/Error";
 import { useTranslation } from "react-i18next";
+import { handleApiError } from "../../helpers/handleApiError";
 
 const initialState = {
   email: "",
@@ -47,21 +48,8 @@ function LoginForm() {
 
       setUserInfo(initialState);
     } catch (err: unknown) {
-      const error = err as ApiErrorResponse;
-
-      if (error.errors) {
-        for (const apiError of error.errors) {
-          setError((prev) => ({
-            ...prev,
-            fields: apiError.field
-              ? [...new Set([...prev.fields, apiError.field])]
-              : prev.fields,
-            messages: apiError.message
-              ? [...prev.messages, apiError.message]
-              : prev.messages
-          }));
-        }
-      }
+      const parsedError = handleApiError(err, t);
+      setError(parsedError);
     }
   };
 
