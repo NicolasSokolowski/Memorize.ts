0e37483a91eb9b700e84abc26c31a57831d7775d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-14 13:02:25 +0200

    calling handleApiError in password reset's handleSubmit catch block

--- client/src/features/user/PasswordReset.tsx
+++ client/src/features/user/PasswordReset.tsx
@@ -9,6 +9,7 @@ import { useAppDispatch } from "../../store/hooks";
 import { errorInitialState, onCancelProp } from "../../types/user";
 import Error from "../../ui/Error";
 import { useTranslation } from "react-i18next";
+import { handleApiError } from "../../helpers/handleApiError";
 
 function PasswordReset({ onCancel }: onCancelProp) {
   const [email, setEmail] = useState("");
@@ -38,21 +39,8 @@ function PasswordReset({ onCancel }: onCancelProp) {
       ).unwrap();
       setEmailHasBeenSent(true);
     } catch (err: unknown) {
-      const error = err as ApiErrorResponse;
-
-      if (error.errors) {
-        for (const apiError of error.errors) {
-          setError((prev) => ({
-            ...prev,
-            fields: apiError.field
-              ? [...new Set([...prev.fields, apiError.field])]
-              : prev.fields,
-            messages: apiError.message
-              ? [...prev.messages, apiError.message]
-              : prev.messages
-          }));
-        }
-      }
+      const parsedError = handleApiError(err, t);
+      setError(parsedError);
     }
   };
 
